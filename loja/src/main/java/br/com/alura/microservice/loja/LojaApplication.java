3644763ad17fc28cad8dc3998edf696f09a77399
nicolas-santanna-campos <nicolassant.anna@gmail.com>
2020-03-08 00:22:24 -0300

    Alterando o nome do pacote.

--- loja/src/main/java/br/com/alura/microservice/loja/LojaApplication.java
+++ loja/src/main/java/br/com/alura/microservice/loja/LojaApplication.java
@@ -1,4 +1,4 @@
-package br.com.alura.microservice.loja.loja;
+package br.com.alura.microservice.loja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
