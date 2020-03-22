2e953e88c2a55bdb46a081954aa17e6a43408acf
nicolas-santanna-campos <nicolassant.anna@gmail.com>
2020-03-22 16:33:56 -0300

    Criando objeto com auxílio da anotação @Autowired.

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.alura.microservice.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,14 +12,16 @@ import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
 @Service
 public class CompraService {
 
+	@Autowired
+	private RestTemplate client;
+
 	public void realizaCompra(CompraDTO compra) {
-		RestTemplate clientRestTemplate = new RestTemplate();
-		ResponseEntity<InfoFornecedorDTO> exchange =  clientRestTemplate.exchange("http://fornecedor/info/" +
-																						  null != compra.getEndereco().getEstado() ? compra.getEndereco().getEstado() : "",
-																						  HttpMethod.GET,
-																						  null,
-																						  InfoFornecedorDTO.class);
-		
+		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange(
+																	"http://fornecedor/info/" + compra.getEndereco().getEstado(),
+																	HttpMethod.GET,
+																	null,
+																	InfoFornecedorDTO.class);
+
 		System.out.println(exchange.getBody().getEndereco());
 	}
 
