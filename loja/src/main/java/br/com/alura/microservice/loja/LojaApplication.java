4298440e49522b43260772153f055a406d674d34
nicolas-santanna-campos <nicolassant.anna@gmail.com>
2020-03-22 16:33:25 -0300

    Atualizando dependências.

--- loja/src/main/java/br/com/alura/microservice/loja/LojaApplication.java
+++ loja/src/main/java/br/com/alura/microservice/loja/LojaApplication.java
@@ -2,12 +2,24 @@ package br.com.alura.microservice.loja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableFeignClients
 public class LojaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
 	}
 
+	@Bean
+	@LoadBalanced
+	public RestTemplate createRestTemplate() {
+		return new RestTemplate();
+	}
 }
--- loja/src/test/java/br/com/alura/microservice/loja/loja/LojaApplicationTests.java
+++ loja/src/test/java/br/com/alura/microservice/loja/loja/LojaApplicationTests.java
@@ -1,6 +1,6 @@
 package br.com.alura.microservice.loja.loja;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
